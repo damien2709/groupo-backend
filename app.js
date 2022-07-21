513d7ebd61df8504c0e9dfbb5ee456b8fba423a9
damien2709 <damien2709@hotmail.fr>
2022-07-21 22:16:00 +0200

    suppression de la fonction initDb et dépendances qui réinitialisait la bdd à chaque redémarrage de l'appli

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const express = require('express')  // on importe le module Express
 const morgan = require('morgan') //j'importe le module-middleware de debugge en local "morgan"
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
-const sequelize = require('./src/db/sequelize') //on importe notre fichier de connexion à la bdd 'sequelize' 
 
 // ------------ EXPRESS + SERVER -------------
 const app = express()  //on crée une instance d'une application Express grâce à la méthode express. Ce sera notre petit serveur web sur lequel va fonctionner notre API REST. 
@@ -16,10 +15,6 @@ app
 .use(favicon(__dirname + '/favicon.ico')) // on utilise le middleware favicon. Ne pas oublié de placer le fichier .ico dans le dossier sinon ca crache !
 .use(morgan('dev')) //on utilise le middleware morgan dans notre API avec l'option "dev" pour optimiser l'affichage des messages de log pour le développement. la fonction 'next()' est automatiquement passée par Morgan, pas besoin de la déclarer !
 .use(bodyParser.json()) // les données transitant par les requêtes entrantes et sortantes HTTP étant toujours sous forme de string, je dois parser les données entrantes pour les transformer en JSON et les utiliser avec javascript ! Je dois stringifier les données sortantes pour les transformer du JSON en string. 
-
-
-// ------------ INITIALISATION DE LA BDD ----------------
-sequelize.initDb()  // On appelle la méthode 'initDb' que l'on a définit dans le ficheir sequelize
 
 
 // ----------------- POINTS DE TERMINAISON ----------------
